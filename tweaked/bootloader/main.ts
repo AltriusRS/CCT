0d583862d231a0053d20c372d58909ce1c57c2b6
Altrius <tom.b.2k2@gmail.com>
2022-08-27 16:15:09 +0100

    update bootloader code to use new CoreOS saving format

--- tweaked/bootloader/main.ts
+++ tweaked/bootloader/main.ts
@@ -1,15 +1,19 @@
-import * as event from "./event";
+import * as events from "./event";
 
-let paste = settings.get("core_os_load")
+let paste = {
+    append: settings.get("coreos.append"),
+    branch: settings.get("coreos.branch"),
+    org: settings.get("coreos.org")
+}
 
-if (paste !== undefined) {
+if (paste.append && paste.branch && paste.org) {
     // Update bootloader
     print("Attempting to download bootloader updates")
     download("startup", "bootloader/main", "tweaked")
 
     // Install software
     print("Attempting to download software updates")
-    download("software", "CoreOS/CoreOS")
+    download("software", paste.append, paste.org, paste.branch)
     print("Downloads finished, launching in 5 seconds")
     os.sleep(5)
     shell.run("software")
@@ -20,12 +24,11 @@ if (paste !== undefined) {
 } else {
     print("Error, coreOS settings corrupted or invalid.")
     print("Reinstalling CoreOS and resetting device.")
-    download("startup", "CoreOS/CoreOS")
+    download("startup", "bootloader/main", "tweaked")
     os.reboot()
 }
 
 function download(path: string, append: string, org: string = "CCTweaked", branch: string = "main") {
     fs.delete(path)
     shell.run("wget", `https://raw.githubusercontent.com/AltriusRS/CCT/${branch}/${org}/${append}.lua`, path);
-
 }
