0a4edc0f0cafb545d462e10034229b240dd58e60
= <thomas@cuisinehub.co.uk>
2025-12-17 23:04:05 +0000

    update the api server to avoid caching the install script to prevent issues

--- Lattice/docs_site/src/routes/pkg/api/index/+server.ts
+++ Lattice/docs_site/src/routes/pkg/api/index/+server.ts
@@ -1,10 +1,19 @@
 const INDEX_URL = 'https://raw.githubusercontent.com/AltriusRS/CCT/main/Lattice/pkg/index.toml';
 
 export const GET = async () => {
-	const manifest = await fetch(INDEX_URL);
+	const manifest = await fetch(INDEX_URL, {
+		cache: 'no-store',
+		headers: {
+			'User-Agent': 'CCT/Lattice',
+			'Cache-Control': 'no-store',
+			Pragma: 'no-cache'
+		}
+	});
 	return new Response(await manifest.text(), {
 		headers: {
-			'Content-Type': 'text/plain'
+			'Content-Type': 'text/plain; charset=utf-8',
+			'Cache-Control': 'no-store',
+			Pragma: 'no-cache'
 		}
 	});
 };
