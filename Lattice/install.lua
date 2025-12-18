b6f2aed404c2b3addc396177261efd55f53a15a7
= <thomas@cuisinehub.co.uk>
2025-12-18 19:46:30 +0000

    fix installer script

--- Lattice/install.lua
+++ Lattice/install.lua
@@ -1,4 +1,4 @@
 -- Simple install.lua (The one on the website)
-shell.run("wget https://lattice-os.cc/pkg/api/package/bin/mesh.lua /bin/mesh.lua")
-shell.run("/bin/mesh.lua bootstrap")
+shell.run("wget", "https://lattice-os.cc/pkg/api/package/bin/mesh.lua", "/bin/mesh.lua")
+shell.run("/bin/mesh.lua", "bootstrap")
 os.reboot()
