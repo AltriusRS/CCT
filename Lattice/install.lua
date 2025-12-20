78233f7d864aa4f5a833dec514b8594bcb63c897
= <thomas@cuisinehub.co.uk>
2025-12-20 14:11:58 +0000

    small tweaks to aid debugging

--- Lattice/install.lua
+++ Lattice/install.lua
@@ -1,6 +1,12 @@
 -- Simple install.lua (The one on the website)
 package.path = package.path .. ";/bin/?.lua;/bin/?/?.lua"
+if fs.exists("/bin/mesh.lua") then
+    print("Deleting old mesh.lua...")
+    fs.delete("/bin/mesh.lua")
+    os.sleep(1)
+end
 shell.run("wget", "https://lattice-os.cc/pkg/api/main/package/bin/mesh/mesh.lua", "/bin/mesh.lua")
+os.sleep(1)
 shell.run("/bin/mesh.lua", "bootstrap")
 os.sleep(10)
 os.reboot()
