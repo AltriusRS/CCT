48f6ebb61bd80a77cf977dbb12a41b7c81c7c019
= <thomas@cuisinehub.co.uk>
2025-12-20 14:06:45 +0000

    added some extra config options (and fixed the doc site)

--- Lattice/docs_site/src/routes/install/+server.ts
+++ Lattice/docs_site/src/routes/install/+server.ts
@@ -1,6 +1,6 @@
 import { REPOSITORY_ROOT } from '$lib/consts';
 
-const URL = REPOSITORY_ROOT + 'main/Lattice/install.lua';
+const URL = REPOSITORY_ROOT + '/main/Lattice/install.lua';
 
 export const GET = async () => {
 	const response = await fetch(URL);
