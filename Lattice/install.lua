e99f38e27afb525f105fee2a5e818cbca1a23e30
= <thomas@cuisinehub.co.uk>
2025-12-20 14:10:07 +0000

    testing fixes for installer

--- Lattice/install.lua
+++ Lattice/install.lua
@@ -2,4 +2,5 @@
 package.path = package.path .. ";/bin/?.lua;/bin/?/?.lua"
 shell.run("wget", "https://lattice-os.cc/pkg/api/main/package/bin/mesh/mesh.lua", "/bin/mesh.lua")
 shell.run("/bin/mesh.lua", "bootstrap")
+os.sleep(10)
 os.reboot()
